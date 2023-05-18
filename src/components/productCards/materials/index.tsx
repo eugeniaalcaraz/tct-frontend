f720517b54d7e0ebf481ee292000d289fc3c0895
Alvaro Muñoz <almunoz@altimetrik.com>
2023-05-18 20:27:58 -0300

    added logic to increase or decrese number of fabrics

--- src/components/productCards/materials/index.tsx
+++ src/components/productCards/materials/index.tsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Box, Tooltip } from "@mui/material";
 import { Container } from "./MaterialsStyles";
 import { Fabrics } from "./fabrics";
 import { Trims } from "./trims";
+import { ControlledDropdown } from "@components/common";
 
 const Materials = () => {
+    const [numberOfFabricsSelected, setNumberOfFabricsSelected] = useState([1]);
+
+    const fabricsArrOptions = [
+        { Id: "1", Description: 1 },
+        { Id: "2", Description: 2 },
+        { Id: "3", Description: 3 },
+        { Id: "4", Description: 4 },
+        { Id: "5", Description: 5 },
+    ];
+
+    const onSelectNumberOfFabrics = (e) => {
+        setNumberOfFabricsSelected(Array.from(Array(Number(e.value)).keys()));
+    };
+
     return (
         <Container>
-            <Fabrics />
+            <ControlledDropdown
+                label="Cantidad de telas"
+                options={fabricsArrOptions}
+                name="cantidadDeTelas"
+                externalOnChange={onSelectNumberOfFabrics}
+            />
+            {numberOfFabricsSelected.map((value) => (
+                <Fabrics key={value} />
+            ))}
+
             <Box className="add">
                 <hr />
                 <Tooltip title="Proximamente 😉" placement="right" arrow>
